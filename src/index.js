9a45f026fe1be532f6a82692c92460f2c1a3f7e2
codenguvl <tripvang123@gmail.com>
2022-06-28 14:27:31 +0700

    update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ app.use("/api", userRoutes);
 app.get("/", (req, res) => {
   res.send("cái lồn má");
 });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 mongoose
   .connect(
     "mongodb+srv://codenguvl:codenguvl@cluster0.ngmugqj.mongodb.net/?retryWrites=true&w=majority"
