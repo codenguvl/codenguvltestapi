536533c9afe81d590796b1d31a084c067b30e633
codenguvl <tripvang123@gmail.com>
2022-06-28 15:50:52 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-/* const bodyParser = require("body-parser");
-const cors = require("cors"); */
+/* const bodyParser = require("body-parser"); */
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const userRoutes = require("./routes/user");
@@ -11,21 +11,13 @@ app.use("/api", userRoutes);
 app.get("/", (req, res) => {
   res.send("Test API");
 });
-/* app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+/* app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); */
-//
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  /* res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); */
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  /* res.header("Access-Control-Allow-Credentials", true); */
-  next();
-});
-//
 mongoose
   .connect(
     "mongodb+srv://codenguvl:codenguvl@cluster0.ngmugqj.mongodb.net/?retryWrites=true&w=majority",
