43cfe0836531f2aeb8a0a8547cce1c73399ec86a
codenguvl <tripvang123@gmail.com>
2022-06-28 15:35:16 +0700

    up

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500/tt.html");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
